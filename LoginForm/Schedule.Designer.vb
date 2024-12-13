6f8d69e386f71efef230fa54c00be3396457cfb9
dan <sisondan390@gmail.com>
2024-12-13 19:38:40 +0800

    i added schedule

--- LoginForm/Schedule.Designer.vb
+++ LoginForm/Schedule.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Schedule
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,32 +20,183 @@ Partial Class Schedule
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnReport = New System.Windows.Forms.Button()
+        Me.btnLogOut = New System.Windows.Forms.Button()
+        Me.btnAccount = New System.Windows.Forms.Button()
+        Me.btnProfile = New System.Windows.Forms.Button()
+        Me.btnNurse = New System.Windows.Forms.Button()
+        Me.btnSchedule = New System.Windows.Forms.Button()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.btnDashboard = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.SteelBlue
+        Me.Panel1.Controls.Add(Me.btnReport)
+        Me.Panel1.Controls.Add(Me.btnLogOut)
+        Me.Panel1.Controls.Add(Me.btnAccount)
+        Me.Panel1.Controls.Add(Me.btnProfile)
+        Me.Panel1.Controls.Add(Me.btnNurse)
+        Me.Panel1.Controls.Add(Me.btnSchedule)
+        Me.Panel1.Controls.Add(Me.btnCreate)
+        Me.Panel1.Controls.Add(Me.btnDashboard)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(-4, -8)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(217, 1013)
+        Me.Panel1.TabIndex = 1
+        '
+        'btnReport
+        '
+        Me.btnReport.FlatAppearance.BorderSize = 0
+        Me.btnReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnReport.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnReport.Location = New System.Drawing.Point(3, 407)
+        Me.btnReport.Name = "btnReport"
+        Me.btnReport.Size = New System.Drawing.Size(215, 69)
+        Me.btnReport.TabIndex = 7
+        Me.btnReport.Text = "Report"
+        Me.btnReport.UseVisualStyleBackColor = True
+        '
+        'btnLogOut
+        '
+        Me.btnLogOut.FlatAppearance.BorderSize = 0
+        Me.btnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLogOut.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnLogOut.Location = New System.Drawing.Point(4, 654)
+        Me.btnLogOut.Name = "btnLogOut"
+        Me.btnLogOut.Size = New System.Drawing.Size(214, 40)
+        Me.btnLogOut.TabIndex = 6
+        Me.btnLogOut.Text = "Log out"
+        Me.btnLogOut.UseVisualStyleBackColor = True
+        '
+        'btnAccount
+        '
+        Me.btnAccount.FlatAppearance.BorderSize = 0
+        Me.btnAccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAccount.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnAccount.Location = New System.Drawing.Point(1, 544)
+        Me.btnAccount.Name = "btnAccount"
+        Me.btnAccount.Size = New System.Drawing.Size(217, 69)
+        Me.btnAccount.TabIndex = 5
+        Me.btnAccount.Text = "Account"
+        Me.btnAccount.UseVisualStyleBackColor = True
+        '
+        'btnProfile
+        '
+        Me.btnProfile.FlatAppearance.BorderSize = 0
+        Me.btnProfile.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnProfile.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnProfile.Location = New System.Drawing.Point(2, 475)
+        Me.btnProfile.Name = "btnProfile"
+        Me.btnProfile.Size = New System.Drawing.Size(215, 69)
+        Me.btnProfile.TabIndex = 2
+        Me.btnProfile.Text = "Profile"
+        Me.btnProfile.UseVisualStyleBackColor = True
+        '
+        'btnNurse
+        '
+        Me.btnNurse.FlatAppearance.BorderSize = 0
+        Me.btnNurse.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnNurse.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnNurse.Location = New System.Drawing.Point(3, 339)
+        Me.btnNurse.Name = "btnNurse"
+        Me.btnNurse.Size = New System.Drawing.Size(214, 69)
+        Me.btnNurse.TabIndex = 4
+        Me.btnNurse.Text = "Nurse"
+        Me.btnNurse.UseVisualStyleBackColor = True
+        '
+        'btnSchedule
+        '
+        Me.btnSchedule.FlatAppearance.BorderSize = 0
+        Me.btnSchedule.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSchedule.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnSchedule.Location = New System.Drawing.Point(3, 271)
+        Me.btnSchedule.Name = "btnSchedule"
+        Me.btnSchedule.Size = New System.Drawing.Size(215, 69)
+        Me.btnSchedule.TabIndex = 3
+        Me.btnSchedule.Text = "Schedule"
+        Me.btnSchedule.UseVisualStyleBackColor = True
+        '
+        'btnCreate
+        '
+        Me.btnCreate.FlatAppearance.BorderSize = 0
+        Me.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCreate.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnCreate.Location = New System.Drawing.Point(3, 203)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(214, 69)
+        Me.btnCreate.TabIndex = 2
+        Me.btnCreate.Text = "Create Shift"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'btnDashboard
+        '
+        Me.btnDashboard.BackColor = System.Drawing.Color.SteelBlue
+        Me.btnDashboard.FlatAppearance.BorderSize = 0
+        Me.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnDashboard.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnDashboard.Location = New System.Drawing.Point(4, 135)
+        Me.btnDashboard.Name = "btnDashboard"
+        Me.btnDashboard.Size = New System.Drawing.Size(214, 69)
+        Me.btnDashboard.TabIndex = 1
+        Me.btnDashboard.Text = "Dashboard"
+        Me.btnDashboard.UseVisualStyleBackColor = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Location = New System.Drawing.Point(4, 4)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(214, 107)
+        Me.Panel2.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(142, 75)
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.Location = New System.Drawing.Point(29, 46)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 28)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Shift Sensei"
         '
         'Schedule
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(1234, 681)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "Schedule"
         Me.Text = "Schedule"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnReport As Button
+    Friend WithEvents btnLogOut As Button
+    Friend WithEvents btnAccount As Button
+    Friend WithEvents btnProfile As Button
+    Friend WithEvents btnNurse As Button
+    Friend WithEvents btnSchedule As Button
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents btnDashboard As Button
+    Friend WithEvents Panel2 As Panel
     Friend WithEvents Label1 As Label
 End Class
